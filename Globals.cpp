8fab063caf91abaaa262502abc377656eef6f2af
Nex <keysatmyfingertips@yahoo.com>
2023-03-29 11:10:16 +0530

    Update Globals.cpp

--- Globals.cpp
+++ Globals.cpp
@@ -8,17 +8,17 @@ namespace Globals {
     extern const float kBG_COLOR_B = 39 / 255.0f;
     extern const float kBG_COLOR_A = 255 / 255.0f;
 
-    extern const float CIRCLE_STEP = 20.0f;
+    extern const float CIRCLE_STEP = 36.0f;
 
     extern const float DEFAULT_ANGLE1 = 100.0f;
     extern const float DEFAULT_ANGLE2 = 100.0f;
-    extern const float LENGTH1 = 1.5f;
+    extern const float LENGTH1 = 1.0f;
     extern const float LENGTH2 = 1.0f;
     extern const float MASS1 = 0.10f;
-    extern const float MASS2 = 0.15f;
+    extern const float MASS2 = 0.10f;
     extern const float ROPE_THICKNESS = 0.05f;
     extern const float MAX_ANGULAR_VELOCITY = 360.0f;
 
     extern const float GRAVITY = 0.0015;
-    extern const int MAX_TRAIL_POINTS = 5000;
+    extern const int MAX_TRAIL_POINTS = 3000;
 }
